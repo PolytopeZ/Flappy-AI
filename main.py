90b7df19900ccee7b6a14f352dacec9e2d97ff6e
polytope <volcania.tumeur@gmail.com>
2025-10-25 22:25:57 +0200

    Changes in order to have multiple bird with a bad heuristic

--- main.py
+++ main.py
@@ -20,16 +20,25 @@ COLOR_BG = (32, 32, 32)
 COLOR_PIPE = (0, 255, 0)
 COLOR_WHITE = (255, 255, 255)
 
+BIRD_COUNT = 10
+
+
+# ******** Utils ********
+def generate_random_color():
+    return (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+
 
 # ******** Classes ********
 class Bird:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color=(255, 255, 0)):
         self.x = x
         self.y = y
         self.tick_count = 0
         self.vel = 0
         self.radius = 15
         self.alive = True
+        self.color = color
+        self.score = 0
 
     def jump(self):
         self.vel = JUMP_STRENGTH
@@ -42,8 +51,14 @@ class Bird:
         if self.y > WIN_HEIGHT or self.y < 0:
             self.alive = False
 
+    def think(self, pipe):
+        # Simple heuristic for now
+        if pipe.x + pipe.width > self.x:
+            if self.y > pipe.y + 10:
+                self.jump()
+
     def draw(self, win):
-        pygame.draw.circle(win, (255, 255, 0), (self.x, self.y), self.radius)
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
 class Pipe:
@@ -75,14 +90,37 @@ class Game:
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         pygame.display.set_caption("Fally UwU")
         self.clock = pygame.time.Clock()
-        self.bird = Bird(100, WIN_HEIGHT // 2)
+        self.birds = [Bird(100, WIN_HEIGHT // 2, generate_random_color())
+                      for _ in range(BIRD_COUNT)]
         self.pipes = [Pipe(WIN_WIDTH + i * 300) for i in range(3)]
         self.score = 0
+        self.generation = 1
         self.font = pygame.font.SysFont("jetbrainsmononlnfpmedium", 24)
 
     def update(self):
+        # Searching for the next pipe
+        next_pipe = None
+        for pipe in self.pipes:
+            # since birds spawn at the same x
+            if pipe.x + pipe.width > self.birds[0].x:
+                next_pipe = pipe
+                break
+
         # Updates entities
-        self.bird.update()
+        # First update birds
+        alive_count = 0
+        for bird in self.birds:
+            if bird.alive:
+                bird.think(next_pipe)  # Todo change this
+                bird.update()
+                # Check if collide
+                for pipe in self.pipes:
+                    if pipe.collide_with(bird):
+                        bird.alive = False
+                if bird.alive:
+                    alive_count += 1
+
+        # Second update pipes
         for pipe in self.pipes:
             pipe.update()
 
@@ -91,28 +129,31 @@ class Game:
             self.pipes.pop(0)
             self.pipes.append(Pipe(WIN_WIDTH + 200))
             self.score += 1
+            for bird in self.birds:
+                bird.score = self.score
 
-        # Check for collisions
-        for pipe in self.pipes:
-            if pipe.collide_with(self.bird):
-                self.bird.alive = False
+        return alive_count
 
     def draw(self):
         self.screen.fill(COLOR_BG)
-
         for pipe in self.pipes:
             pipe.draw(self.screen)
-        self.bird.draw(self.screen)
+        for bird in self.birds:
+            if bird.alive:
+                bird.draw(self.screen)
 
-        text = self.font.render(f"Score: {self.score}", True, COLOR_WHITE)
+        text = self.font.render(
+            f"Score: {self.score}, Gen: {self.generation}", True, COLOR_WHITE)
         self.screen.blit(text, (10, 10))
 
         pygame.display.flip()
 
     def reset(self):
-        self.bird = Bird(100, WIN_HEIGHT // 2)
+        self.birds = [Bird(100, WIN_HEIGHT // 2, generate_random_color())
+                      for _ in range(BIRD_COUNT)]
         self.pipes = [Pipe(WIN_WIDTH + i * 300) for i in range(3)]
         self.score = 0
+        self.generation += 1
 
     def run(self):
         running = True
@@ -125,16 +166,11 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    if not self.bird.alive:
-                        self.reset()
-                    else:
-                        self.bird.jump()
+            alive_count = self.update()
 
-            if self.bird.alive:
-                self.update()
-            else:
+            if alive_count == 0:
                 self.reset()
+
             self.draw()
 
 
